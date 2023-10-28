26ebdfbdb6369075682a869cd8c4f521715100ce
Tobias <tobiasfunes@hotmail.com.ar>
2023-10-28 10:24:44 -0300

    subiendo clase Nodo

--- src/entities/Node.java
+++ src/entities/Node.java
@@ -1,5 +1,22 @@
 package entities;
 
-public class Node {
-    
+public class Node<AnyType> {
+    private Integer heigth;
+    private AnyType element;
+    private Node<AnyType> left;
+    private Node<AnyType> right;
+
+
+    public Node(AnyType element) {
+        this.heigth = 1;
+        this.element = element;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(AnyType element, Node<AnyType> left, Node<AnyType> right) {
+        this.element = element;
+        this.left = left;
+        this.right = right;
+    }
 }
