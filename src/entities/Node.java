8fd0ef18e128bd3cb5a93c67c41764f1632fd31a
Agustin Brambilla <agustinbram@gmail.com>
2023-11-01 21:53:40 -0300

    Modified node class, we thought to use class node as a product and eliminate product's class

--- src/entities/Node.java
+++ src/entities/Node.java
@@ -5,6 +5,7 @@ package entities;
  */
 public class Node{
     public String element;
+    public int stock;
     /**
      * Constructs a new node with the specified element.
      *
@@ -12,5 +13,40 @@ public class Node{
      */
     public Node(String element) {
         this.element = element;
+        this.stock = 0;
     }
+
+    /**
+     * Returns the element of the product
+     * @return elment's node
+     */
+    public String getElement() {
+        return element;
+    }
+
+    /**
+     * Sets the element of the product.
+     * @param element to be setted.
+     */
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    /**
+     * Returns stock of the product.
+     * @return stock's node.
+     */
+    public int getStock() {
+        return stock;
+    }
+
+    /**
+     * Sets stock of the product.
+     * @param stock to be setted.
+     */
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    
 }
