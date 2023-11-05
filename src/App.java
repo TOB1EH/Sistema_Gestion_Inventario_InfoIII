98bf2679813705d44ef123a63b7ddf44bcc9e56f
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2023-11-05 10:30:58 -0300

    Funcionalidad Añadir producto V1.0

--- src/App.java
+++ src/App.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 import entities.AVLTree;
 import entities.List;
+import entities.Product;
+import exceptions.ProductNotFoundException;
 
 public class App {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         AVLTree productTree = new AVLTree();
         List productHistory = new List();
         switch(menu()) {
             case 1:
-                System.out.println("Ingrese el nombre del producto que desea agregar.");        
+                addProduct(productTree, productHistory);
                 break;
             case 2:
                 System.out.println("Ingrese el nombre del producto que desea eliminar.");
@@ -28,7 +31,6 @@ public class App {
 }
 
     private static int menu() {
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("\n" + //
                 "──────────────────────────────────────────\n" + //
@@ -51,9 +53,87 @@ public class App {
         System.out.println("Si desea buscar un producto, ingrese el numero 3");
         System.out.println("Si desea mostrar el inventario completo de productos, ingrese el numero 4");
         System.out.println("Si desea salir, ingrese el numero 5");
-        int op =scanner.nextInt();
-        scanner.close();
+        int op =Integer.parseInt(scanner.nextLine());
         return op;
     }
-    
+
+public static void addProduct( AVLTree productTree, List productList)
+{
+    String productName="";
+    do{
+        System.out.println("Ingrese el nombre del producto que desea agregar:");
+        productName = scanner.nextLine();
+        if(productName.equals(""))
+            System.out.println("Debes ingresar un nombre de producto!");
+    }while(productName.equals(""));
+    Product product;
+
+    //busqueda
+    try//buscar en el arbol 
+    {
+        product = productTree.searchProduct(productName.toLowerCase().replaceAll("\\s+", ""));
+        System.out.println("Encontrado: \n" + product);
+        incrementStock(product);
+
+    }
+    catch(ProductNotFoundException e)//el produto no se encontro en el arbol
+    {
+        //busqueda en la lista
+        System.out.println("->CONTROL FLOW SOUT: Producto no encontrado en arbol");//sout auxiliar
+        try
+        {
+            product = productList.searchProduct(productName.toLowerCase().replaceAll("\\s+", ""));
+            System.out.println("Encontrado: \n" + product);
+            incrementStock(product);
+
+        }
+        catch(ProductNotFoundException d)//producto no se encontro en la lista
+        {
+            String op="";
+            do
+            {
+                System.out.println("El producto: " + productName + 
+                " no se encuentra registrado en el sistema\n Deseea añadirlo?(s/n)\n");
+                op = scanner.nextLine().toLowerCase().replaceAll("\\s+", "");
+                switch (op)
+                {
+                    case "s":
+                        product = new Product(productName.toLowerCase().replaceAll("\\s+", ""), 0);
+                        incrementStock(product);
+                        //insertar en arbol y lista
+                        productTree.insertProduct(product);
+                        productList.insertNode(product);
+                        System.out.println("Producto añadido con exito!");
+                        break;
+                    case "n":
+                        break;
+                    default:
+                        System.out.println("Invalido");
+                        break;
+                }
+            }while(!op.equals("n") && !op.equals("s"));
+        }
+    }
+}
+
+    public static void incrementStock(Product product)
+    {
+        int stock =0;
+        do
+        {
+            System.out.println("Ingresa el stock a añadir: ");
+            try
+            {
+                stock = Integer.parseInt(scanner.nextLine());
+            }
+            catch(NumberFormatException nfe)
+            {
+                System.out.println("Debes ingresar un valor numerico");
+            }
+            if(stock == 0)
+                System.out.println("Debes ingresar un valor superior a 0");
+        } while(stock > 0);
+        
+        product.stock += stock;
+    }
 }
