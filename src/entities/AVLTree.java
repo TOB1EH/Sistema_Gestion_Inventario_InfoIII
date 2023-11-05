46c1f46831c7290a18526362919e311230962b14
Agustin Brambilla <agustinbram@gmail.com>
2023-11-05 09:27:07 -0300

    Beginning with optimization of deleteNode of avltree

--- src/entities/AVLTree.java
+++ src/entities/AVLTree.java
@@ -185,6 +185,24 @@ public class AVLTree {
         root.height = max(height(root.left), height(root.right)) + 1;
         int balance = getBalance(root);
 
+        if(balance > 1) {
+            if(getBalance(root.left) >= 0) {
+                return rightRotate(root);
+            } else {
+                root.left = leftRotate(root.left);
+                return rightRotate(root);
+            }
+        }
+
+        if(balance < -1) {
+            if(getBalance(root.right) <= 0) {
+                return leftRotate(root);    
+            } else {
+                root.right = rightRotate(root.right);
+                return leftRotate(root);
+            }
+        }
+
         if (balance > 1 && getBalance(root.left) >= 0) {
             return rightRotate(root);
         }
